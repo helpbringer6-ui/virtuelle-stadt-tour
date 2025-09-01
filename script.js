5a54572cb344d9e514fd62f977423f9166fb7bcb
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-01 10:40:45 -0500

    Update console log message for clarity.

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('The Virtual City Tour is ready!');
+    console.log('The Virtual City Tour has been successfully loaded and is ready for you!');
 });
